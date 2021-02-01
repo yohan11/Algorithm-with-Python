e0f9a5eab3e2848595c87d984babe7450cd99963
yohan11 <40304565+yohan11@users.noreply.github.com>
2021-02-02 01:29:21 +0900

    Create 4836.py

---
+++ SW Expert Academy/Programming Intermediate/LIST2/4836.py
@@ -0,0 +1,11 @@
+#
+T = int(input())
+whiteboard = [[0 for _ in range(10)] for _ in range(10)]
+
+for i in range(1,T+1):
+    N=int(input())
+    for area in range(1,N+1):
+        x1,y1,x2,y2,color=map(int,input().split())
+        
+
+
