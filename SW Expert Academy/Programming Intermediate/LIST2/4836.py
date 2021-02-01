19bf3c40f1c9d2e658ca1fa4d2d9f5c172097651
yohan11 <40304565+yohan11@users.noreply.github.com>
2021-02-02 01:30:08 +0900

    Update 4836.py

--- SW Expert Academy/Programming Intermediate/LIST2/4836.py
+++ SW Expert Academy/Programming Intermediate/LIST2/4836.py
@@ -1,4 +1,4 @@
-#
+#색칠하기
 T = int(input())
 whiteboard = [[0 for _ in range(10)] for _ in range(10)]
 
@@ -6,6 +6,3 @@ for i in range(1,T+1):
     N=int(input())
     for area in range(1,N+1):
         x1,y1,x2,y2,color=map(int,input().split())
-        
-
-
