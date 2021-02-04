2337d6d648472f16451f45b05fd2a0757aec61b6
yohan11 <40304565+yohan11@users.noreply.github.com>
2021-02-04 23:51:51 +0900

    Update 4836.py

--- SW Expert Academy/Programming Intermediate/LIST2/4836.py
+++ SW Expert Academy/Programming Intermediate/LIST2/4836.py
@@ -1,8 +1,19 @@
 #색칠하기
 T = int(input())
-whiteboard = [[0 for _ in range(10)] for _ in range(10)]
 
-for i in range(1,T+1):
-    N=int(input())
-    for area in range(1,N+1):
-        x1,y1,x2,y2,color=map(int,input().split())
+for TC in range(1, T+1):
+    red = []
+    blue = []
+    N = int(input())
+    for area in range(1, N+1):
+        x1, y1, x2, y2, color = map(int, input().split())
+        for y in range(y1, y2+1):
+            for x in range(x1, x2+1):
+                if color == 1:
+                    red.append((x, y))
+                elif color == 2:
+                    blue.append((x, y))
+    red = set(red)
+    blue = set(blue)
+    purple = red.intersection(blue)
+    print("#%d %d" % (TC, len(purple)))
