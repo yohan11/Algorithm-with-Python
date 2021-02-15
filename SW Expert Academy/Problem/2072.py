54a71de52919bd23978b0d9a2bced1abe8a44679
yohan11 <40304565+yohan11@users.noreply.github.com>
2021-02-15 22:54:42 +0900

    Update 2072.py

--- SW Expert Academy/Problem/2072.py
+++ SW Expert Academy/Problem/2072.py
@@ -1,1 +1,14 @@
-dd
+#홀수만 더하기
+
+T = int(input())
+
+for TC in range(1, T+1):
+    num = list(map(int, input().split()))
+    sumNum = []
+    sum = 0
+    for i in range(10):
+        if num[i] % 2 == 1:
+            sumNum.append(num[i])
+    for i in sumNum:
+        sum += i
+    print(f"#{TC} {sum}")
