4ca041171821dd9986eb67add83ed6c72d889184
yohan11 <yipco77@naver.com>
2023-11-09 02:07:29 +0900

    그리디-모험가길드

--- 동빈나/그리디/모험가길드.py
+++ 동빈나/그리디/모험가길드.py
@@ -1,16 +1,30 @@
-from collections import deque
+# from collections import deque
+
+# N = int(input())
+# X = deque(sorted(list(map(int, input().split()))))
+# answer = []
+
+# while True:
+#     if len(X) < X[-1]:
+#         break
+
+#     group = []
+#     for j in range(X[0]):
+#         group.append(X.popleft())
+#     answer.append(group)
+
+# print(len(answer))
+
 
 N = int(input())
-X = deque(sorted(list(map(int, input().split()))))
-answer = []
+X = sorted(list(map(int, input().split())))
 
-while True:
-    if len(X) < X[-1]:
-        break
+answer = 0
+member = 0
 
-    group = []
-    for j in range(X[0]):
-        group.append(X.popleft())
-    answer.append(group)
-
-print(len(answer))
+for i in X:
+    member += 1
+    if member >= i:
+        answer += 1
+        member = 0
+print(answer)
